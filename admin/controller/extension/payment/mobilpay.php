24c1aaab7937308321b1de5e24edde39590b8bc8
Navid <torabazary@yahoo.com>
2020-10-18 02:25:59 +0300

    Self Validation Validation

--- admin/controller/extension/payment/mobilpay.php
+++ admin/controller/extension/payment/mobilpay.php
@@ -1,9 +1,12 @@
 <?php
 error_reporting(E_ALL);
 class ControllerExtensionPaymentMobilpay extends Controller {
-	private $error = array(); 
-
+	private $error = array();
+    protected $_domain;
 	public function index() {
+	    // get User Token
+        $data['user_token'] = $this->session->data['user_token'];
+
 		$this->load->language('extension/payment/mobilpay');
 
 		$this->document->setTitle($this->language->get('heading_title'));
@@ -131,6 +134,109 @@ class ControllerExtensionPaymentMobilpay extends Controller {
 			$data['payment_mobilpay_sort_order'] = $this->config->get('payment_mobilpay_sort_order');
 		}
 
+        if (isset($this->request->post['payment_mobilpay_live_pub_key'])) {
+            $data['payment_mobilpay_live_pub_key'] = $this->request->post['payment_mobilpay_live_pub_key'];
+        } else {
+            $data['payment_mobilpay_live_pub_key'] = $this->config->get('payment_mobilpay_live_pub_key');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_live_pri_key'])) {
+            $data['payment_mobilpay_live_pri_key'] = $this->request->post['payment_mobilpay_live_pri_key'];
+        } else {
+            $data['payment_mobilpay_live_pri_key'] = $this->config->get('payment_mobilpay_live_pri_key');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_sand_pub_key'])) {
+            $data['payment_mobilpay_sand_pub_key'] = $this->request->post['payment_mobilpay_sand_pub_key'];
+        } else {
+            $data['payment_mobilpay_sand_pub_key'] = $this->config->get('payment_mobilpay_sand_pub_key');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_sand_pri_key'])) {
+            $data['payment_mobilpay_sand_pri_key'] = $this->request->post['payment_mobilpay_sand_pri_key'];
+        } else {
+            $data['payment_mobilpay_sand_pri_key'] = $this->config->get('payment_mobilpay_sand_pri_key');
+        }
+
+
+        if (isset($this->request->post['payment_mobilpay_conditions_complete_description'])) {
+            $data['payment_mobilpay_conditions_complete_description'] = $this->request->post['payment_mobilpay_conditions_complete_description'];
+        } else {
+            $data['payment_mobilpay_conditions_complete_description'] = $this->config->get('payment_mobilpay_conditions_complete_description');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_conditions_prices_currency'])) {
+            $data['payment_mobilpay_conditions_prices_currency'] = $this->request->post['payment_mobilpay_conditions_prices_currency'];
+        } else {
+            $data['payment_mobilpay_conditions_prices_currency'] = $this->config->get('payment_mobilpay_conditions_prices_currency');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_conditions_has_ssl'])) {
+            $data['payment_mobilpay_conditions_has_ssl'] = $this->request->post['payment_mobilpay_conditions_has_ssl'];
+        } else {
+            $data['payment_mobilpay_conditions_has_ssl'] = $this->config->get('payment_mobilpay_conditions_has_ssl');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_conditions_forbidden_business'])) {
+            $data['payment_mobilpay_conditions_forbidden_business'] = $this->request->post['payment_mobilpay_conditions_forbidden_business'];
+        } else {
+            $data['payment_mobilpay_conditions_forbidden_business'] = $this->config->get('payment_mobilpay_conditions_forbidden_business');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_clarity_contact'])) {
+            $data['payment_mobilpay_clarity_contact'] = $this->request->post['payment_mobilpay_clarity_contact'];
+        } else {
+            $data['payment_mobilpay_clarity_contact'] = $this->config->get('payment_mobilpay_clarity_contact');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_terms_conditions_url'])) {
+            $data['payment_mobilpay_terms_conditions_url'] = $this->request->post['payment_mobilpay_terms_conditions_url'];
+        } else {
+            $data['payment_mobilpay_terms_conditions_url'] = $this->config->get('payment_mobilpay_terms_conditions_url');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_privacy_policy_url'])) {
+            $data['payment_mobilpay_privacy_policy_url'] = $this->request->post['payment_mobilpay_privacy_policy_url'];
+        } else {
+            $data['payment_mobilpay_privacy_policy_url'] = $this->config->get('payment_mobilpay_privacy_policy_url');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_delivery_policy_url'])) {
+            $data['payment_mobilpay_delivery_policy_url'] = $this->request->post['payment_mobilpay_delivery_policy_url'];
+        } else {
+            $data['payment_mobilpay_delivery_policy_url'] = $this->config->get('payment_mobilpay_delivery_policy_url');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_return_cancel_policy_url'])) {
+            $data['payment_mobilpay_return_cancel_policy_url'] = $this->request->post['payment_mobilpay_return_cancel_policy_url'];
+        } else {
+            $data['payment_mobilpay_return_cancel_policy_url'] = $this->config->get('payment_mobilpay_return_cancel_policy_url');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_gdpr_policy_url'])) {
+            $data['payment_mobilpay_gdpr_policy_url'] = $this->request->post['payment_mobilpay_gdpr_policy_url'];
+        } else {
+            $data['payment_mobilpay_gdpr_policy_url'] = $this->config->get('payment_mobilpay_gdpr_policy_url');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_image_visa_logo_link'])) {
+            $data['payment_mobilpay_image_visa_logo_link'] = $this->request->post['payment_mobilpay_image_visa_logo_link'];
+        } else {
+            $data['payment_mobilpay_image_visa_logo_link'] = $this->config->get('payment_mobilpay_image_visa_logo_link');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_image_master_logo_link'])) {
+            $data['payment_mobilpay_image_master_logo_link'] = $this->request->post['payment_mobilpay_image_master_logo_link'];
+        } else {
+            $data['payment_mobilpay_image_master_logo_link'] = $this->config->get('payment_mobilpay_image_master_logo_link');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_image_netopia_logo_link'])) {
+            $data['payment_mobilpay_image_netopia_logo_link'] = $this->request->post['payment_mobilpay_image_netopia_logo_link'];
+        } else {
+            $data['payment_mobilpay_image_netopia_logo_link'] = $this->config->get('payment_mobilpay_image_netopia_logo_link');
+        }
+
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
@@ -149,4 +255,189 @@ class ControllerExtensionPaymentMobilpay extends Controller {
 		
 		return !$this->error;
 	}
+
+    protected function getDomain() {
+//	    $this->_domain = 'https://'.$_SERVER['HTTP_HOST'].DIRECTORY_SEPARATOR; // Temporary Use HTTP
+	    $this->_domain = 'http://'.$_SERVER['HTTP_HOST'].DIRECTORY_SEPARATOR;
+    }
+
+    protected function hasDeclarations() {
+        $isValid = 1;
+        $declarations = array (
+            'completeDescription' => $this->request->post['payment_mobilpay_conditions_complete_description'],
+            'priceCurrency'       => $this->request->post['payment_mobilpay_conditions_prices_currency'],
+            'contactInfo'         => $this->request->post['payment_mobilpay_clarity_contact'],
+            'forbiddenBusiness'   => $this->request->post['payment_mobilpay_conditions_forbidden_business']
+        );
+        foreach ($declarations as $key => $value) {
+            if($value !== 'yes'){
+                $isValid = 0;
+                $error[$key] = $key . " , is not valid!";
+            }
+        }
+        return array(
+            'status' => $isValid,
+            'error'  => isset($error) ? $error: '',
+        );
+    }
+
+    protected function hasMandatoryPages() {
+        $isValid = 1;
+        $mandatoryPages = array(
+            'termsAndConditions'    => $this->request->post['payment_mobilpay_terms_conditions_url'],
+            'privacyPolicy'         => $this->request->post['payment_mobilpay_privacy_policy_url'],
+            'deliveryPolicy'        => $this->request->post['payment_mobilpay_delivery_policy_url'],
+            'returnAndCancelPolicy' => $this->request->post['payment_mobilpay_return_cancel_policy_url'],
+            'gdprPolicy' => $_POST['payment_mobilpay_gdpr_policy_url']
+        );
+        $pages = [];
+        foreach ($mandatoryPages as $key => $value) {
+            $checkUrl = $this->checkUrlValidation($value);
+            if(!$checkUrl['status']){
+                $isValid = 0;
+                $error[$key]           = $value . " , is not valid URL!";
+            }
+            $pages[$key]['status'] = $checkUrl['status'];
+            $pages[$key]['code']   = $checkUrl['code'];
+        }
+
+        return array(
+            'status' => $isValid,
+            'result' => $pages,
+            'error'  => isset($error) ? $error: '',
+        );
+    }
+
+    public function checkImageValidation ($image) {
+        $exist = 1;
+        $imagePath = $_SERVER['DOCUMENT_ROOT'].'/'.$image;
+        if(!file_exists($imagePath)) {
+            $exist = 0;
+        }
+        return $exist;
+    }
+
+    protected function hasMandatoryImages() {
+        $isValid = 1;
+        $MandatoryImages = array(
+            'visaLogoLink'    => $this->request->post['payment_mobilpay_image_visa_logo_link'],
+            'masterLogoLink'  => $this->request->post['payment_mobilpay_image_master_logo_link'],
+            'netopiaLogoLink' => $this->request->post['payment_mobilpay_image_netopia_logo_link']
+        );
+        $img = [];
+        foreach ($MandatoryImages as $key => $value) {
+            $checkImage = $this->checkImageValidation($value);
+            if(!$checkImage){
+                $isValid = 0;
+                $error[$key] = $value . " , is not valid image!";
+            }
+            $img[$key]['status'] = $checkImage;
+        }
+
+        return array(
+            'status' => $isValid,
+            'result' => $img,
+            'error'  => isset($error) ? $error: '',
+        );
+    }
+
+    protected function checkUrlValidation($url) {
+        $isValid = false;
+        $code = null;
+        $url= $this->_domain.$url;
+        if(isset($url) && is_string($url) && preg_match('/^http(s)?:\/\/[a-z0-9-]+(\.[a-z0-9-]+)*(:[0-9]+)?(\/.*)?$/i', $url)){
+            $ch = curl_init($url);
+            if($ch !== false) {
+                curl_setopt($ch, CURLOPT_HEADER         ,true);
+                curl_setopt($ch, CURLOPT_NOBODY         ,true);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER ,true);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION ,false);
+                curl_setopt($ch, CURLOPT_MAXREDIRS      ,10);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,5);
+                curl_setopt($ch, CURLOPT_TIMEOUT        ,6);
+                curl_exec($ch);
+                if(!curl_errno($ch)) {
+                    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                    $redirectNumber = curl_getinfo($ch, CURLINFO_REDIRECT_COUNT);
+                    if($code == 200 && $redirectNumber==0) {$isValid = true;}
+                }
+            }
+        }
+
+        return array(
+            'status' =>  $isValid,
+            'code'   => $code
+        );
+    }
+
+    protected function hasSsl() {
+        $isValid = 1;
+//        TEMPORRY DOMAIN NAME
+      $domain = 'https://netopia-payments.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
+//      $domain = 'http://mtyplast.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
+//      $domain = 'http://mtypdfgddfgdfgdfgdfglast.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
+//      $domain = $this->_domain; // Main Domain
+
+        $stream = stream_context_create (array("ssl" => array("capture_peer_cert" => true)));
+        if($read = @fopen($domain, "rb", false, $stream)) {
+            $cont = stream_context_get_params($read);
+            if(array_key_exists('peer_certificate', $cont["options"]["ssl"])){
+                $var = $cont["options"]["ssl"]["peer_certificate"];
+                $result = (!is_null($var)) ? 1 : 0;
+            } else{
+                $isValid = 0;
+                $result = 0;
+                $error['hasSSL'] = $this->_domain . ", doesn't have valid SSL Certification";
+            }
+        } else {
+            $isValid = 0;
+            $result = 0;
+            $error['hasSSL']= $this->_domain . ", is not a valid domain";
+        }
+
+    return array(
+        'status' => $isValid,
+        'result' => $result,
+        'error'  => isset($error) ? $error: '',
+    );
+    }
+
+    public function selfValidation() {
+        $this->getDomain();
+        $declareValidation  = $this->hasDeclarations();
+        $pagesVerification  = $this->hasMandatoryPages();
+        $imgVerification    = $this->hasMandatoryImages();
+        $sslVerification    = $this->hasSsl();
+
+        $validateResult = array(
+            'declare' => $declareValidation,
+            'urls'    => $pagesVerification,
+            'images'  => $imgVerification,
+            'ssl'     => $sslVerification
+        );
+
+        $isValid = true;
+        foreach ($validateResult as $key => $value) {
+            if($value['status'] != 1) {
+                $isValid = false;
+            }
+        }
+
+
+        if($isValid) {
+            $jsonResponse = array(
+                'status'  =>  true,
+                'msg'     => 'All fields are valid! Please click on button "Send request to NETOPIA Payments to verify" to continue',
+                'details' => $validateResult);
+        } else {
+            $jsonResponse = array(
+                'result'  =>  false,
+                'msg'     => "There is still problem on Validation\n. ",
+                'result' => $validateResult );
+        }
+        /*
+        * Send response to JS
+        */
+        echo (json_encode($jsonResponse));
+    }
 }
--- admin/language/ro-ro/extension/payment/mobilpay.php
+++ admin/language/ro-ro/extension/payment/mobilpay.php
@@ -10,6 +10,10 @@ $_['text_edit']                     = 'Edit Netopia mobilPay';
 $_['text_mobilpay']        = '<a onclick="window.open(\'http://www.mobilpay.ro\');"><img src="view/image/payment/mobilpay.gif" alt="Netopia mobilPay" title="Netopia mobilPay" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_live']                      = 'Live';
 $_['text_sandbox']                   = 'Sandbox';
+$_['text_yes']                       = 'Yes';
+$_['text_no']                        = 'No';
+$_['text_maybe']                     = 'Maybe';
+$_['text_chose']                     = 'choose an answer';
 
 // Entry
 $_['entry_signature']    				   = 'Account Signature';
@@ -23,7 +27,82 @@ $_['entry_order_status_credit']		 	   = 'Order Status Credit:';
 $_['entry_geo_zone']     				   = 'Geo Zone:';
 $_['entry_status']       				   = 'Status:';
 $_['entry_sort_order']   				   = 'Sort Order:';
-$_['entry_total']             = 'Total';
+$_['entry_total']                          = 'Total';
+$_['entry_cert_live_pub_key_file']         = 'Live Public Key';
+$_['entry_cert_live_pri_key_file']         = 'Live Private Key';
+$_['entry_cert_sand_pub_key_file']         = 'Sandbox Public Key';
+$_['entry_cert_sand_pri_key_file']         = 'Sandbox Private Key';
+
+$_['declare_complete_description']         = 'Declare, have complete description';
+$_['declare_complete_description_detail']  = 'Declare that there is a clear and complete description of the goods and services that we will sell';
+
+$_['declare_prices_currency']              = 'Declare, have clearly prices and currency';
+$_['declare_prices_currency_detail']       = 'Declare that the prices and currency are clear displayed for the goods / services';
+
+
+$_['declare_clarity_contact']              = 'Declare, clarity of contact info';
+$_['declare_clarity_contact_detail']       = 'Declare that, the contact details of the company (SC, CUI, address, telephone, fax / e-mail,...) are clearly displayed on the website';
+
+$_['declare_ssl_certificate']              = 'Declare, to use SSL Certificate valid';
+$_['declare_ssl_certificate_detail']       = 'If your website has a valid SSL certificate';
+
+
+$_['declare_forbidden_business']           = 'Declare, of business / activities';
+$_['declare_forbidden_business_detail']    = 'Declare that we are not do trade on the list above also in none of the forbidden business / services';
+$_['forbidden_item_1']    = 'domeniul farmaceutic: produse sintetice si naturiste, inclusiv suplimente alimentare';
+$_['forbidden_item_2']    = 'tutun, inclusiv narghilea, tigari electronice si consumabile';
+$_['forbidden_item_3']    = 'gambling sub toate formele (jocuri de noroc, pariuri sportive si nu numai, quizz-uri in care se percepe taxa de participare si se castiga bani, alte jocuri cu castiguri in bani - bingo, forex, site-uri de licitatii, etc)';
+$_['forbidden_item_4']    = 'materiale pentru adulti: inchiriere/vanzare casete video de profil, (video)chat, dating, escorte';
+$_['forbidden_item_5']    = 'alcool (exceptie vin si bere)';
+$_['forbidden_item_6']    = 'articole si produse de vanatoare (arme albe si arme de foc, cu munitie clasica si comprimata)';
+$_['forbidden_item_7']    = 'videostreaming (cu exceptia cazului in care exista acte doveditoare: sunteti persoana care realizeaza aceste video-uri sau detineti drepturi de autor asupra lor astrologie, tarot, etc.';
+
+$_['terms_conditions_url']              = 'Terms and conditions';
+$_['entry_terms_conditions_page_url']   = 'Entry your terms and conditions url';
+$_['terms_conditions_url_detail']       = 'ex. somewhere/terms_and_conditions OR terms_and_conditions.html | without your actual domain name.';
+
+$_['privacy_policy_url']                = 'Privacy policy';
+$_['entry_privacy_policy_page_url']     = 'Entry your Privacy Policy url';
+$_['privacy_policy_url_detail']         = 'ex. somewhere/privacy_policy OR privacy_policy.html | without your actual domain name.';
+
+$_['delivery_policy_url']               = 'Delivery policy';
+$_['entry_delivery_page_url']           = 'Entry your Delivery Policy url';
+$_['delivery_policy_url_detail']        = 'ex. somewhere/delivery_policy OR ex. delivery_policy.html | without your actual domain name.';
+
+$_['return_cancel_policy_url']          = 'Return / Cancel policy';
+$_['entry_return_cancel_page_url']      = 'Entry your Return or Cancel page url';
+$_['return_cancel_policy_url_detail']   = 'ex. somewhere/return_cancel OR ex. return_cancel.html | without your actual domain name.';
+
+$_['gdpr_policy_url']                   = 'GDPR policy';
+$_['gdpr_page_url']                     = 'Entry your GDPR page url';
+$_['gdpr_policy_url_detail']            = 'ex. somewhere/gdpr_policy OR ex. gdpr_policy.html | without your actual domain name.';
+
+$_['image_visa_logo_link']             = 'Visa card logo';
+$_['image_visa_logo_link_placeholder'] = 'Entry path of Visa card logo';
+$_['image_visa_logo_link_detail']      = 'ex. images/visa.png | without your actual domain name.';
+
+$_['image_master_logo_link']             = 'Mastercard logo';
+$_['image_master_logo_link_placeholder'] = 'Entry path of Mastercard logo';
+$_['image_master_logo_link_detail']      = 'ex. images/mastercard.jpg | without your actual domain name.';
+
+$_['image_netopia_logo_link']             = 'NETOPIA Payments logo';
+$_['image_netopia_logo_link_placeholder'] = 'Entry path of NETOPIA Payments logo';
+$_['image_netopia_logo_link_detail']      = 'ex. images/netopia-patements.svg | without your actual domain name.';
+
+// Title
+$_['ntp_plugin_configurations']         = 'Plugin Configurations';
+$_['certification_key_upload']          = 'Certification keys';
+$_['ntp_agreements']                    = 'NETOPIA Payments Agrements';
+$_['ntp_conditions_agreements']         = 'Conditions / Agreements';
+$_['ntp_not_accpeted_business']         = 'Not accpeted business / activities';
+$_['ntp_mandatory_pages']               = 'Mandatory pages URL';
+$_['ntp_mandatory_logo']                = 'Mandatory logo links';
+
+// Button
+$_['send_ntp_verify']                           = 'Send request to NETOPIA Payments to verify';
+$_['self_validation']                           = 'Self Validation';
+$_['ask_ntp_verification']                           = 'Ask NETOPIA Payements to Verify';
+$_['do_self_verification']                           = 'Make sure everything is alright';
 
 // Help
 $_['help_total']					= 'The checkout total the order must reach before this payment method becomes active.';
