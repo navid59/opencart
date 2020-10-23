d039333ff112f9e6fd7e1a8751c47e9c5c1f47d7
azar <azar@azar.maghsoodi>
2020-10-23 13:10:30 +0300

    controller

--- admin/controller/extension/payment/mobilpay.php
+++ admin/controller/extension/payment/mobilpay.php
@@ -1,9 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 class ControllerExtensionPaymentMobilpay extends Controller {
-	private $error = array();
+    private $error = array();
     protected $_domain;
     protected $outEnvKey;
     protected $outEncData;
@@ -13,139 +10,139 @@ class ControllerExtensionPaymentMobilpay extends Controller {
     const ERROR_ENCRYPT_DATA          = 0x10000002;
 
 
-	public function index() {
-	    // get User Token
+    public function index() {
+        // get User Token
         $data['user_token'] = $this->session->data['user_token'];
 
-		$this->load->language('extension/payment/mobilpay');
+        $this->load->language('extension/payment/mobilpay');
 
-		$this->document->setTitle($this->language->get('heading_title'));
-		
-		$this->load->model('setting/setting');
+        $this->document->setTitle($this->language->get('heading_title'));
+
+        $this->load->model('setting/setting');
 
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
             $this->model_setting_setting->editSetting('payment_mobilpay', array_merge($this->request->post));
             $this->session->data['success'] = $this->language->get('text_success');
 
-			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
-		}
+            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
+        }
 
         //die(print_r($this->request->files));
 
- 		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
-		
- 		if (isset($this->error['signature'])) {
-			$data['error_signature'] = $this->error['signature'];
-		} else {
-			$data['error_signature'] = '';
-		}
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
 
-		$data['breadcrumbs'] = array();
+        if (isset($this->error['signature'])) {
+            $data['error_signature'] = $this->error['signature'];
+        } else {
+            $data['error_signature'] = '';
+        }
 
-   		$data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-      		   		);
-					
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_extension'),
-			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true)
-		);
+        $data['breadcrumbs'] = array();
 
-   		$data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('extension/payment/mobilpay', 'user_token=' . $this->session->data['user_token'], true),
-      		);
-				
-		$data['action'] = $this->url->link('extension/payment/mobilpay', 'user_token=' . $this->session->data['user_token'], true);
-		
-		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true);
+        $data['breadcrumbs'][] = array(
+            'text'      => $this->language->get('text_home'),
+            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+        );
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_extension'),
+            'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true)
+        );
+
+        $data['breadcrumbs'][] = array(
+            'text'      => $this->language->get('heading_title'),
+            'href'      => $this->url->link('extension/payment/mobilpay', 'user_token=' . $this->session->data['user_token'], true),
+        );
+
+        $data['action'] = $this->url->link('extension/payment/mobilpay', 'user_token=' . $this->session->data['user_token'], true);
+
+        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true);
 
 
-		if (isset($this->request->post['payment_mobilpay_signature'])) {
-			$data['payment_mobilpay_signature'] = $this->request->post['payment_mobilpay_signature'];
-		} else {
-			$data['payment_mobilpay_signature'] = $this->config->get('payment_mobilpay_signature');
-		}
-		
-		if (isset($this->request->post['payment_mobilpay_total'])) {
-			$data['payment_mobilpay_total'] = $this->request->post['payment_mobilpay_total'];
-		} else {
-			$data['payment_mobilpay_total'] = $this->config->get('payment_mobilpay_total');
-		}
-		
-		if (isset($this->request->post['payment_mobilpay_test'])) {
-			$data['payment_mobilpay_test'] = $this->request->post['payment_mobilpay_test'];
-		} else {
-			$data['payment_mobilpay_test'] = $this->config->get('payment_mobilpay_test');
-		}
-				
-		if (isset($this->request->post['payment_mobilpay_order_status_id'])) {
-			$data['payment_mobilpay_order_status_id'] = $this->request->post['payment_mobilpay_order_status_id'];
-		} else {
-			$data['payment_mobilpay_order_status_id'] = $this->config->get('payment_mobilpay_order_status_id'); 
-		}
-		
-		if (isset($this->request->post['payment_mobilpay_order_status_confirmed_pending_id'])) {
-			$data['payment_mobilpay_order_status_confirmed_pending_id'] = $this->request->post['payment_mobilpay_order_status_confirmed_pending_id'];
-		} else {
-			$data['payment_mobilpay_order_status_confirmed_pending_id'] = $this->config->get('payment_mobilpay_order_status_confirmed_pending_id'); 
-		}
-		
-		if (isset($this->request->post['payment_mobilpay_order_status_paid_pending_id'])) {
-			$data['payment_mobilpay_order_status_paid_pending_id'] = $this->request->post['payment_mobilpay_order_status_paid_pending_id'];
-		} else {
-			$data['payment_mobilpay_order_status_paid_pending_id'] = $this->config->get('payment_mobilpay_order_status_paid_pending_id'); 
-		}
-		
-		if (isset($this->request->post['payment_mobilpay_order_status_paid_id'])) {
-			$data['payment_mobilpay_order_status_paid_id'] = $this->request->post['payment_mobilpay_order_status_paid_id'];
-		} else {
-			$data['payment_mobilpay_order_status_paid_id'] = $this->config->get('payment_mobilpay_order_status_paid_id'); 
-		}
+        if (isset($this->request->post['payment_mobilpay_signature'])) {
+            $data['payment_mobilpay_signature'] = $this->request->post['payment_mobilpay_signature'];
+        } else {
+            $data['payment_mobilpay_signature'] = $this->config->get('payment_mobilpay_signature');
+        }
 
-		if (isset($this->request->post['payment_mobilpay_order_status_canceled_id'])) {
-			$data['payment_mobilpay_order_status_canceled_id'] = $this->request->post['payment_mobilpay_order_status_canceled_id'];
-		} else {
-			$data['payment_mobilpay_order_status_canceled_id'] = $this->config->get('payment_mobilpay_order_status_canceled_id'); 
-		}
+        if (isset($this->request->post['payment_mobilpay_total'])) {
+            $data['payment_mobilpay_total'] = $this->request->post['payment_mobilpay_total'];
+        } else {
+            $data['payment_mobilpay_total'] = $this->config->get('payment_mobilpay_total');
+        }
 
-		if (isset($this->request->post['payment_mobilpay_order_status_credit_id'])) {
-			$data['payment_mobilpay_order_status_credit_id'] = $this->request->post['payment_mobilpay_order_status_credit_id'];
-		} else {
-			$data['payment_mobilpay_order_status_credit_id'] = $this->config->get('payment_mobilpay_order_status_credit_id'); 
-		}		
+        if (isset($this->request->post['payment_mobilpay_test'])) {
+            $data['payment_mobilpay_test'] = $this->request->post['payment_mobilpay_test'];
+        } else {
+            $data['payment_mobilpay_test'] = $this->config->get('payment_mobilpay_test');
+        }
 
-		$this->load->model('localisation/order_status');
-		
-		$data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
-		
-		if (isset($this->request->post['payment_mobilpay_geo_zone_id'])) {
-			$data['payment_mobilpay_geo_zone_id'] = $this->request->post['payment_mobilpay_geo_zone_id'];
-		} else {
-			$data['payment_mobilpay_geo_zone_id'] = $this->config->get('payment_mobilpay_geo_zone_id'); 
-		} 
-		
-		$this->load->model('localisation/geo_zone');
-										
-		$data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
-		
-		if (isset($this->request->post['payment_mobilpay_status'])) {
-			$data['payment_mobilpay_status'] = $this->request->post['payment_mobilpay_status'];
-		} else {
-			$data['payment_mobilpay_status'] = $this->config->get('payment_mobilpay_status');
-		}
-		
-		if (isset($this->request->post['payment_mobilpay_sort_order'])) {
-			$data['payment_mobilpay_sort_order'] = $this->request->post['payment_mobilpay_sort_order'];
-		} else {
-			$data['payment_mobilpay_sort_order'] = $this->config->get('payment_mobilpay_sort_order');
-		}
+        if (isset($this->request->post['payment_mobilpay_order_status_id'])) {
+            $data['payment_mobilpay_order_status_id'] = $this->request->post['payment_mobilpay_order_status_id'];
+        } else {
+            $data['payment_mobilpay_order_status_id'] = $this->config->get('payment_mobilpay_order_status_id');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_order_status_confirmed_pending_id'])) {
+            $data['payment_mobilpay_order_status_confirmed_pending_id'] = $this->request->post['payment_mobilpay_order_status_confirmed_pending_id'];
+        } else {
+            $data['payment_mobilpay_order_status_confirmed_pending_id'] = $this->config->get('payment_mobilpay_order_status_confirmed_pending_id');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_order_status_paid_pending_id'])) {
+            $data['payment_mobilpay_order_status_paid_pending_id'] = $this->request->post['payment_mobilpay_order_status_paid_pending_id'];
+        } else {
+            $data['payment_mobilpay_order_status_paid_pending_id'] = $this->config->get('payment_mobilpay_order_status_paid_pending_id');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_order_status_paid_id'])) {
+            $data['payment_mobilpay_order_status_paid_id'] = $this->request->post['payment_mobilpay_order_status_paid_id'];
+        } else {
+            $data['payment_mobilpay_order_status_paid_id'] = $this->config->get('payment_mobilpay_order_status_paid_id');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_order_status_canceled_id'])) {
+            $data['payment_mobilpay_order_status_canceled_id'] = $this->request->post['payment_mobilpay_order_status_canceled_id'];
+        } else {
+            $data['payment_mobilpay_order_status_canceled_id'] = $this->config->get('payment_mobilpay_order_status_canceled_id');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_order_status_credit_id'])) {
+            $data['payment_mobilpay_order_status_credit_id'] = $this->request->post['payment_mobilpay_order_status_credit_id'];
+        } else {
+            $data['payment_mobilpay_order_status_credit_id'] = $this->config->get('payment_mobilpay_order_status_credit_id');
+        }
+
+        $this->load->model('localisation/order_status');
+
+        $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+
+        if (isset($this->request->post['payment_mobilpay_geo_zone_id'])) {
+            $data['payment_mobilpay_geo_zone_id'] = $this->request->post['payment_mobilpay_geo_zone_id'];
+        } else {
+            $data['payment_mobilpay_geo_zone_id'] = $this->config->get('payment_mobilpay_geo_zone_id');
+        }
+
+        $this->load->model('localisation/geo_zone');
+
+        $data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+
+        if (isset($this->request->post['payment_mobilpay_status'])) {
+            $data['payment_mobilpay_status'] = $this->request->post['payment_mobilpay_status'];
+        } else {
+            $data['payment_mobilpay_status'] = $this->config->get('payment_mobilpay_status');
+        }
+
+        if (isset($this->request->post['payment_mobilpay_sort_order'])) {
+            $data['payment_mobilpay_sort_order'] = $this->request->post['payment_mobilpay_sort_order'];
+        } else {
+            $data['payment_mobilpay_sort_order'] = $this->config->get('payment_mobilpay_sort_order');
+        }
 
         if (isset($this->request->post['payment_mobilpay_sand_pri_key'])) {
             $data['payment_mobilpay_sand_pri_key'] = $this->request->post['payment_mobilpay_sand_pri_key'];
@@ -293,12 +290,12 @@ class ControllerExtensionPaymentMobilpay extends Controller {
             $data['payment_mobilpay_image_netopia_logo_link'] = $this->config->get('payment_mobilpay_image_netopia_logo_link');
         }
 
-		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
 
-		$this->response->setOutput($this->load->view('extension/payment/mobilpay', $data));
-	}
+        $this->response->setOutput($this->load->view('extension/payment/mobilpay', $data));
+    }
 
     protected function uploadFile($file,$allowTypes,$fieldName)
     {
@@ -319,15 +316,15 @@ class ControllerExtensionPaymentMobilpay extends Controller {
 
 
     private function validate() {
-		if (!$this->user->hasPermission('modify', 'extension/payment/mobilpay')) {
-			$this->error['warning'] = $this->language->get('error_permission');
-		}
+        if (!$this->user->hasPermission('modify', 'extension/payment/mobilpay')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
 
-		if (!$this->request->post['payment_mobilpay_signature']) {
-			$this->error['signature'] = $this->language->get('error_signature');
-		}
+        if (!$this->request->post['payment_mobilpay_signature']) {
+            $this->error['signature'] = $this->language->get('error_signature');
+        }
         if(!empty($this->request->files['payment_mobilpay_live_pub_key_file']['name'])) {
-           $this->uploadFile($this->request->files['payment_mobilpay_live_pub_key_file'],['cer'],'payment_mobilpay_live_pub_key_file');
+            $this->uploadFile($this->request->files['payment_mobilpay_live_pub_key_file'],['cer'],'payment_mobilpay_live_pub_key_file');
         }
 
         if(!empty($this->request->files['payment_mobilpay_live_pri_key_file']['name'])) {
@@ -340,13 +337,13 @@ class ControllerExtensionPaymentMobilpay extends Controller {
         if(!empty($this->request->files['payment_mobilpay_sand_pri_key_file']['name'])) {
             $this->uploadFile($this->request->files['payment_mobilpay_sand_pri_key_file'],['key'],'payment_mobilpay_sand_pri_key_file');
         }
-		
-		return !$this->error;
-	}
+
+        return !$this->error;
+    }
 
     protected function getDomain() {
 //	    $this->_domain = 'https://'.$_SERVER['HTTP_HOST'].DIRECTORY_SEPARATOR; // Temporary Use HTTP
-	    $this->_domain = 'http://'.$_SERVER['HTTP_HOST'].DIRECTORY_SEPARATOR;
+        $this->_domain = 'http://'.$_SERVER['HTTP_HOST'].DIRECTORY_SEPARATOR;
     }
 
     protected function hasDeclarations() {
@@ -462,7 +459,7 @@ class ControllerExtensionPaymentMobilpay extends Controller {
     protected function hasSsl() {
         $isValid = 1;
 //        TEMPORRY DOMAIN NAME
-      $domain = 'https://netopia-payments.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
+        $domain = 'https://netopia-payments.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
 //      $domain = 'http://mtyplast.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
 //      $domain = 'http://mtypdfgddfgdfgdfgdfglast.com'.DIRECTORY_SEPARATOR; // TEMPORARY to Test SSL
 //      $domain = $this->_domain; // Main Domain
@@ -484,11 +481,11 @@ class ControllerExtensionPaymentMobilpay extends Controller {
             $error['hasSSL']= $this->_domain . ", is not a valid domain";
         }
 
-    return array(
-        'status' => $isValid,
-        'result' => $result,
-        'error'  => isset($error) ? $error: '',
-    );
+        return array(
+            'status' => $isValid,
+            'result' => $result,
+            'error'  => isset($error) ? $error: '',
+        );
     }
 
     public function selfValidation() {
